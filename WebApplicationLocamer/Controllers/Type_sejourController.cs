1c9b136fc1df4c925aa566ff22d6214859ea48ee
lestien.nathan <lestien.nathan@R20903>
2017-05-15 11:05:25 +0200

    Ajoutez des fichiers projet.

---
+++ WebApplicationLocamer/Controllers/Type_sejourController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationLocamer.Models;
+
+namespace WebApplicationLocamer.Controllers
+{
+    public class Type_sejourController : Controller
+    {
+        private Locamer1Entities db = new Locamer1Entities();
+
+        // GET: Type_sejour
+        public ActionResult Index()
+        {
+            return View(db.Type_sejour.ToList());
+        }
+
+        // GET: Type_sejour/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Type_sejour type_sejour = db.Type_sejour.Find(id);
+            if (type_sejour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type_sejour);
+        }
+
+        // GET: Type_sejour/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Type_sejour/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_Tsejour,libelle_Tsejour,coefficient_Tsejour")] Type_sejour type_sejour)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Type_sejour.Add(type_sejour);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(type_sejour);
+        }
+
+        // GET: Type_sejour/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Type_sejour type_sejour = db.Type_sejour.Find(id);
+            if (type_sejour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type_sejour);
+        }
+
+        // POST: Type_sejour/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_Tsejour,libelle_Tsejour,coefficient_Tsejour")] Type_sejour type_sejour)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(type_sejour).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(type_sejour);
+        }
+
+        // GET: Type_sejour/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Type_sejour type_sejour = db.Type_sejour.Find(id);
+            if (type_sejour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type_sejour);
+        }
+
+        // POST: Type_sejour/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Type_sejour type_sejour = db.Type_sejour.Find(id);
+            db.Type_sejour.Remove(type_sejour);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplicationLocamer/Controllers/Type_tarifController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationLocamer.Models;
+
+namespace WebApplicationLocamer.Controllers
+{
+    public class Type_tarifController : Controller
+    {
+        private Locamer1Entities db = new Locamer1Entities();
+
+        // GET: Type_tarif
+        public ActionResult Index()
+        {
+            return View(db.Type_tarif.ToList());
+        }
+
+        // GET: Type_tarif/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Type_tarif type_tarif = db.Type_tarif.Find(id);
+            if (type_tarif == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type_tarif);
+        }
+
+        // GET: Type_tarif/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Type_tarif/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_Ttarif,libelle_Ttarif,prixJour_Ttarif")] Type_tarif type_tarif)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Type_tarif.Add(type_tarif);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(type_tarif);
+        }
+
+        // GET: Type_tarif/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Type_tarif type_tarif = db.Type_tarif.Find(id);
+            if (type_tarif == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type_tarif);
+        }
+
+        // POST: Type_tarif/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_Ttarif,libelle_Ttarif,prixJour_Ttarif")] Type_tarif type_tarif)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(type_tarif).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(type_tarif);
+        }
+
+        // GET: Type_tarif/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Type_tarif type_tarif = db.Type_tarif.Find(id);
+            if (type_tarif == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type_tarif);
+        }
+
+        // POST: Type_tarif/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Type_tarif type_tarif = db.Type_tarif.Find(id);
+            db.Type_tarif.Remove(type_tarif);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
